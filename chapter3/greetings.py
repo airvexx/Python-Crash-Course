b76e382cd26eefccbe6709161f9fed5dc925c859
airvexx <xaviersherwood@icloud.com>
2017-10-30 21:00:32 -0700

    chapter 3
    chapter 3 try it yourself exercises

---
+++ chapter3/greetings.py
@@ -0,0 +1,14 @@
+names = ['hagbard celine', 'robert anton wilson', 'israel regardie', 'aleister crowley', 'lao tzu', 'carl sagan']
+message_0 = "Greetings, " + names[0].title() +"!" + " How are you today" + "?"
+message_1 = "Greetings, " + names[1].title() +"!" + " How are you today" + "?"
+message_2 = "Greetings, " + names[2].title() +"!" + " How are you today" + "?"
+message_3 = "Greetings, " + names[3].title() +"!" + " How are you today" + "?"
+message_4 = "Greetings, " + names[4].title() +"!" + " How are you today" + "?"
+message_5 = "Greetings, " + names[-1].title() +"!" + " How are you today" + "?"
+
+print(message_0)
+print(message_1)
+print(message_2)
+print(message_3)
+print(message_4)
+print(message_5)
