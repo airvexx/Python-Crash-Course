d6c712ac3a792ef1ddd4b9cd1fc4b78a3ac9f77c
airvexx <xaviersherwood@icloud.com>
2017-10-30 22:32:09 -0700

    Chapter 3
    Try it yourself exercise

--- chapter3/yourownlist.py
+++ chapter3/yourownlist.py
@@ -1,2 +1,18 @@
-message = "Hello"
-print(message)
+## Airvexx
+## Access item from a list with an individual message for each item
+
+transport = ['bicycle', 'car', 'jetpack', 'hoverboard', 'airplane', 'magic carpet']
+
+message_0 = "Riding a " + transport[0] + " is a lot of fun" + "!"
+message_1 = "A " + transport[1] + " is ok" + ", " + "I guess" + "."
+message_2 = "Would it not be awesome to have a " + transport[2] + "?"
+message_3 = "Marty rides a " + transport[3] + "."
+message_4 = "How long have you been inside a " + transport[4] + "?"
+message_5 = "Aladdin flies a " + transport[5] + "."
+
+print(message_0)
+print(message_1)
+print(message_2)
+print(message_3)
+print(message_4)
+print(message_5)
