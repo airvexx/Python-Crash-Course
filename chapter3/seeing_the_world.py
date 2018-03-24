cbb095e659b7a175801d8c06928627bb3c55a29b
Xavier <xaviersherwood@icloud.com>
2018-03-24 12:57:58 -0700

    Try it Yourself
    Seeing the World

---
+++ chapter3/seeing_the_world.py
@@ -0,0 +1,31 @@
+visit = ['spain', 'germany', 'england', 'portugal', 'france' ]
+print("\nOriginal order:")
+print(visit)
+
+print("\nAlphabetical:")
+print(sorted(visit))
+
+print("\nOriginal order:")
+print(visit)
+
+print("\nReverse alphabetical:")
+print(sorted(visit, reverse=True))
+
+print("\nOriginal order:")
+print(visit)
+
+print("\nReversed:")
+visit.reverse()
+print(visit)
+
+print("\nOriginal Order:")
+visit.reverse()
+print(visit)
+
+print("\nAlphabetical:")
+visit.sort()
+print(visit)
+
+print("\nReverse alphabetical:")
+visit.sort(reverse=True)
+print(visit)
